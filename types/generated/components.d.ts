279a89135f16029c0e618705d30a9cbf4b64d423
lucas <lucas.solcher@digitalservice.bund.de>
2024-10-30 11:01:53 +0100

    Remove alttext since we can use the built-in alttext

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -11,7 +11,6 @@ export interface SharedVisualisierung extends Struct.ComponentSchema {
     Bild: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     Beschreibung: Schema.Attribute.Blocks;
     VisualisierungsArt: Schema.Attribute.String;
-    BildAlternativText: Schema.Attribute.String;
   };
 }
 
