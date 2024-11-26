d1200c76eb60c4e989f9477eaa993b07e84919ed
lucas <lucas.solcher@digitalservice.bund.de>
2024-11-26 14:23:45 +0100

    GraphQL settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+
+module.exports = () => ({
+    graphql: {
+        enabled: true,
+        config: {
+            playgroundAlways: true,
+            defaultLimit: 10,
+            maxLimit: 20,
+            apolloServer: {
+                tracing: true,
+                cache: 'bounded'
+            },
+        }
+    }
+})
