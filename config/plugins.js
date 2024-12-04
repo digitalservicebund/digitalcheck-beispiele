e9fcfa766305be753fd341f508791bfacec5a36c
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-12-04 12:14:42 +0100

    Add ApolloCacheControl

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,22 @@
+const ApolloServerPluginCacheControl = require('@apollo/server/plugin/cacheControl').ApolloServerPluginCacheControl;
+
 module.exports = () => ({
     graphql: {
         enabled: true,
         config: {
             playgroundAlways: true,
             defaultLimit: 20,
+            apolloServer: {
+                tracing: true,
+                plugins: [
+                    ApolloServerPluginCacheControl({
+                        // Cache everything for 1 second by default.
+                        defaultMaxAge: 3600,
+                        // Don't send the `cache-control` response header.
+                        // calculateHttpHeaders: false,
+                      }),
+                ]
+            },
         },
     },
 });
