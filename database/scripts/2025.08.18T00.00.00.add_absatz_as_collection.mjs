2c074de8d2473882c3e21707ec72f18363eb44ac
basti.paintner <basti.paintner@digitalservice.bund.de>
2025-08-19 14:29:57 +0200

    WIP build migrations

--- database/scripts/2025.08.18T00.00.00.add_absatz_as_collection.mjs
+++ database/scripts/2025.08.18T00.00.00.add_absatz_as_collection.mjs
@@ -1,12 +1,6 @@
 import { GraphQLClient, gql } from "graphql-request";
 
-async function main() {
-  const client = new GraphQLClient("http://localhost:1337/graphql", {
-    headers: {
-      Authorization: `Bearer ${process.env.STRAPI_TOKEN}`, // needs API token
-    },
-  });
-
+async function addAbsaetze(client) {
   // Get all absatz component entries
   const getParagraphsWithAbsaetze = gql`
     query GetParagraphsWithAbsaetze {
@@ -46,8 +40,6 @@ async function main() {
     }))
   );
 
-  console.log(absaetze.length);
-
   // Add absatz as collection
   const addAbsatz = gql`
     mutation AddAbsatz($data: AbsatzInput!) {
@@ -68,9 +60,115 @@ async function main() {
   `;
 
   for (const absatz of absaetze) {
-    console.log(JSON.stringify(absatz, null, 2));
     await client.request(addAbsatz, { data: absatz });
   }
+}
+
+async function addBeispielvorhabenVisualisierungReference(client) {
+  const getVisualisierungenAndRegelungsvorhaben = gql`
+    query Digitalchecks {
+      digitalchecks {
+        Visualisierungen {
+          documentId
+        }
+        Regelungsvorhaben {
+          documentId
+        }
+      }
+    }
+  `;
+
+  let visualisierungenAndRegelungsvorhaben;
+  try {
+    visualisierungenAndRegelungsvorhaben = await client.request(
+      getVisualisierungenAndRegelungsvorhaben
+    );
+  } catch (error) {
+    console.error("Error fetching paragraphs:", error);
+    throw error;
+  }
+
+  const updateRegelungsvorhaben = gql`
+    mutation Mutation($documentId: ID!, $data: RegelungsvorhabenInput!) {
+      updateRegelungsvorhaben(documentId: $documentId, data: $data) {
+        Visualisierungen {
+          documentId
+        }
+      }
+    }
+  `;
+
+  for (const digitalcheck of visualisierungenAndRegelungsvorhaben.digitalchecks) {
+    await client.request(updateRegelungsvorhaben, {
+      documentId: digitalcheck.Regelungsvorhaben.documentId,
+      data: {
+        Visualisierungen: digitalcheck.Visualisierungen.map(
+          ({ documentId }) => documentId
+        ),
+      },
+    });
+  }
+}
+
+async function addBeispielvorhabenPrinzipReference(client) {
+  const getPrinzipienAndRegelungsvorhaben = gql`
+    query QueryPrinzipien {
+      prinzips {
+        GuteUmsetzungen {
+          Regelungsvorhaben {
+            documentId
+          }
+        }
+        documentId
+      }
+    }
+  `;
+
+  let prinzipienAndRegelungsvorhaben;
+  try {
+    prinzipienAndRegelungsvorhaben = await client.request(
+      getPrinzipienAndRegelungsvorhaben
+    );
+  } catch (error) {
+    console.error("Error fetching paragraphs:", error);
+    throw error;
+  }
+
+  const updatePrinzipien = gql`
+    mutation Mutation($documentId: ID!, $data: PrinzipInput!) {
+      updatePrinzip(documentId: $documentId, data: $data) {
+        Beispielvorhaben {
+          documentId
+        }
+      }
+    }
+  `;
+
+  for (const prinzip of prinzipienAndRegelungsvorhaben.prinzips) {
+    await client.request(updatePrinzipien, {
+      documentId: prinzip.document_id,
+      data: {
+        Beispielvorhaben: prinzip.GuteUmsetzungen.map(
+          (digitalcheck) => digitalcheck.Regelungsvorhaben.document_id
+        ),
+      },
+    });
+  }
+}
+
+/**
+ * STRAPI_TOKEN=your_api_token npm run add-absatz-as-collection
+ */
+async function main() {
+  const client = new GraphQLClient("http://localhost:1337/graphql", {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_TOKEN}`, // needs API token
+    },
+  });
+
+  await addAbsaetze(client);
+  await addBeispielvorhabenVisualisierungReference(client);
+  await addBeispielvorhabenPrinzipReference(client);
 }
 
 main().catch(console.error);
