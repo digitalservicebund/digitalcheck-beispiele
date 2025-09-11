a7cb8ab5e64326861d3c2789940310f78417e56f
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-09-11 12:52:39 +0200

    Adapt script to use json data to update relations

--- database/scripts/2025-09-10-rename-regelungsvorhaben.mjs
+++ database/scripts/2025-09-10-rename-regelungsvorhaben.mjs
@@ -1,38 +1,36 @@
 import { GraphQLClient, gql } from "graphql-request";
+import Regelungsvorhabens from "../data/Regelungsvorhaben.json" assert { type: "json" };
+import Prinzipien from "../data/Prinzipien.json" assert { type: "json" };
+
+const regelungsvorhabens = Regelungsvorhabens.data.regelungsvorhabens;
+const prinzipien = Prinzipien.data.prinzips;
+
+console.log(regelungsvorhabens.length, prinzipien.length);
 
 const beispielvorhabenMap = new Map();
 
 const copyRegelungsvorhabenToBeispielvorhaben = async (client) => {
-  const getRegelungsvorhabens = gql`
-    query Regelungsvorhaben {
-      regelungsvorhabens(status: DRAFT) {
+  // delete all beispielvorhaben
+  const getBeispielvorhaben = gql`
+    query Beispielvorhaben {
+      beispielvorhabens {
         documentId
-        Titel
-        Ressort
-        NKRStellungnahmeLink
-        DIPVorgang
-        NKRNummer
-        URLBezeichnung
-        Rechtsgebiet
-        VeroeffentlichungsDatum
-        Digitalchecks {
-          documentId
-        }
-        LinkRegelungstext
-        GesetzStatus
-        Manteltext
-        Paragraphen {
-          documentId
-        }
-        Visualisierungen {
-          documentId
-        }
-        publishedAt
       }
     }
   `;
-
-  const { regelungsvorhabens } = await client.request(getRegelungsvorhabens);
+  const deleteBeispielvorhaben = gql`
+    mutation DeleteBeispielvorhaben($documentId: ID!) {
+      deleteBeispielvorhaben(documentId: $documentId) {
+        documentId
+      }
+    }
+  `;
+  const { beispielvorhabens } = await client.request(getBeispielvorhaben);
+  for (const documentId of beispielvorhabens.map(
+    ({ documentId }) => documentId
+  )) {
+    await client.request(deleteBeispielvorhaben, { documentId });
+  }
 
   const createBeispielvorhaben = gql`
     mutation CreateBeispielvorhaben($data: BeispielvorhabenInput!) {
@@ -65,6 +63,7 @@ const copyRegelungsvorhabenToBeispielvorhaben = async (client) => {
 
   for (const regelungsvorhaben of regelungsvorhabens) {
     const { documentId, ...rest } = regelungsvorhaben;
+    console.log(publishedAt);
 
     const res = await client.request(createBeispielvorhaben, {
       data: {
@@ -83,7 +82,39 @@ const copyRegelungsvorhabenToBeispielvorhaben = async (client) => {
 
     beispielvorhabenMap.set(documentId, res.createBeispielvorhaben.documentId);
   }
+
+  const updatePrinzip = gql`
+    mutation UpdatePrinzip($documentId: ID!, $data: PrinzipInput!) {
+      updatePrinzip(documentId: $documentId, data: $data) {
+        Beispielvorhaben {
+          documentId
+        }
+      }
+    }
+  `;
+
+  for (const prinzip of prinzipien) {
+    console.log({
+      documentId: prinzip.documentId,
+      data: {
+        Beispielvorhaben: prinzip.Beispielvorhaben.map(
+          ({ documentId }) => beispielvorhabenMap.get(documentId)
+        ),
+      },
+    });
+    await client.request(updatePrinzip, {
+      documentId: prinzip.documentId,
+      data: {
+        Beispielvorhaben: prinzip.Beispielvorhaben.map(
+          ({ documentId }) => beispielvorhabenMap.get(documentId)
+        ),
+      },
+    });
+  }
+
 };
+
+
 
 /**
  * STRAPI_TOKEN=your_api_token node ./database/scripts/2025-09-10-rename-regelungsvorhaben.mjs [url]
