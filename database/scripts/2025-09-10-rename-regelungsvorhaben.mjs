ad08339f1c2f073c013393601a70d2a289755622
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-09-11 17:54:15 +0200

    Copy remaining fields to be renamed

--- database/scripts/2025-09-10-rename-regelungsvorhaben.mjs
+++ database/scripts/2025-09-10-rename-regelungsvorhaben.mjs
@@ -1,15 +1,15 @@
 import { GraphQLClient, gql } from "graphql-request";
-import Regelungsvorhabens from "../data/Regelungsvorhaben.json" assert { type: "json" };
 import Prinzipien from "../data/Prinzipien.json" assert { type: "json" };
+import Regelungsvorhabens from "../data/Regelungsvorhaben.json" assert { type: "json" };
 
 const regelungsvorhabens = Regelungsvorhabens.data.regelungsvorhabens;
 const prinzipien = Prinzipien.data.prinzips;
 
-console.log(regelungsvorhabens.length, prinzipien.length);
-
 const beispielvorhabenMap = new Map();
 
-const copyRegelungsvorhabenToBeispielvorhabenAndUpdatePrinzipien = async (client) => {
+const copyRegelungsvorhabenToBeispielvorhabenAndUpdatePrinzipien = async (
+  client
+) => {
   const createBeispielvorhaben = gql`
     mutation CreateBeispielvorhaben($data: BeispielvorhabenInput!) {
       createBeispielvorhaben(data: $data) {
@@ -71,27 +71,109 @@ const copyRegelungsvorhabenToBeispielvorhabenAndUpdatePrinzipien = async (client
   `;
 
   for (const prinzip of prinzipien) {
-    console.log({
-      documentId: prinzip.documentId,
-      data: {
-        Beispielvorhaben: prinzip.Beispielvorhaben.map(
-          ({ documentId }) => beispielvorhabenMap.get(documentId)
-        ),
-      },
-    });
     await client.request(updatePrinzip, {
       documentId: prinzip.documentId,
       data: {
-        Beispielvorhaben: prinzip.Beispielvorhaben.map(
-          ({ documentId }) => beispielvorhabenMap.get(documentId)
+        Beispielvorhaben: prinzip.Beispielvorhaben.map(({ documentId }) =>
+          beispielvorhabenMap.get(documentId)
         ),
       },
     });
   }
-
 };
 
+const copyPrinzipienAnwendungToPrinzipAspekt = async (client) => {
+  const getPrinzips = gql`
+    query Prinzips {
+      prinzips(status: DRAFT) {
+        documentId
+        PrinzipienAnwendung {
+          Title
+          Text
+          Questions
+          WordingExample
+          Beispiel {
+            documentId
+          }
+        }
+      }
+    }
+  `;
+  const { prinzips } = await client.request(getPrinzips);
 
+  const updatePrinzipAspekt = gql`
+    mutation Prinzip($documentId: ID!, $data: PrinzipInput!) {
+      updatePrinzip(documentId: $documentId, data: $data) {
+        PrinzipAspekt {
+          Titel
+          Text
+          Leitfragen
+          Formulierungsbeispiel
+          Beispiel {
+            documentId
+          }
+        }
+      }
+    }
+  `;
+
+  for (const prinzip of prinzips) {
+    await client.request(updatePrinzipAspekt, {
+      documentId: prinzip.documentId,
+      data: {
+        PrinzipAspekt: prinzip.PrinzipienAnwendung.map((anwendung) => ({
+          Titel: anwendung.Title,
+          Text: anwendung.Text,
+          Leitfragen: anwendung.Questions,
+          Formulierungsbeispiel: anwendung.WordingExample,
+          Beispiel: anwendung.Beispiel?.documentId,
+        })),
+      },
+    });
+  }
+};
+
+const copyWarumGutToErklärung = async (client) => {
+  const getAbsaetze = gql`
+    query Absatz {
+      absaetze(status: DRAFT) {
+        documentId
+        PrinzipErfuellungen {
+          Prinzip {
+            documentId
+          }
+          WarumGut
+        }
+      }
+    }
+  `;
+
+  const { absaetze } = await client.request(getAbsaetze);
+  const updateAbsatz = gql`
+    mutation UpdateAbsatz($documentId: ID!, $data: AbsatzInput!) {
+      updateAbsatz(documentId: $documentId, data: $data) {
+        PrinzipErfuellungen {
+          Prinzip {
+            documentId
+          }
+          Erklaerung
+        }
+      }
+    }
+  `;
+  for (const absatz of absaetze) {
+    await client.request(updateAbsatz, {
+      documentId: absatz.documentId,
+      data: {
+        PrinzipErfuellungen: absatz.PrinzipErfuellungen.map((erfuellung) => ({
+          Prinzip: erfuellung.Prinzip.documentId,
+          Erklaerung: erfuellung.WarumGut,
+          WarumGut: erfuellung.WarumGut,
+        })),
+      },
+    });
+  }
+};
 
 /**
  * STRAPI_TOKEN=your_api_token node ./database/scripts/2025-09-10-rename-regelungsvorhaben.mjs [url]
@@ -105,6 +187,8 @@ async function main() {
   });
 
   await copyRegelungsvorhabenToBeispielvorhabenAndUpdatePrinzipien(client);
+  await copyPrinzipienAnwendungToPrinzipAspekt(client);
+  await copyWarumGutToErklärung(client);
 }
 
 main().catch(console.error);
