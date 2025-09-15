c645d8624d9ba1165f7acb06b9ba2850334daf7f
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-09-16 00:35:19 +0200

    Revert "Remove regelungsvorhaben from schema"
    This reverts commit 46246bf5d4a3db43211b11afc3f3bd5f2c0cbd03.

---
+++ src/api/regelungsvorhaben/controllers/regelungsvorhaben.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * regelungsvorhaben controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::regelungsvorhaben.regelungsvorhaben');
---
+++ src/api/regelungsvorhaben/services/regelungsvorhaben.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * regelungsvorhaben service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::regelungsvorhaben.regelungsvorhaben');
