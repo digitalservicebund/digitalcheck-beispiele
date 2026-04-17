7d24513851b4e23d93c97f92c898f3189b9d5a64
basti.paintner <basti.paintner@digitalservice.bund.de>
2026-04-17 09:55:00 +0200

    feat(online-docu): add hilfetext to prinzip

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -701,6 +701,7 @@ export interface ApiPrinzipPrinzip extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    Hilfetext: Schema.Attribute.Blocks;
     Kurzbeschreibung: Schema.Attribute.Blocks;
     Kurzbezeichnung: Schema.Attribute.String &
       Schema.Attribute.Required &
@@ -1029,6 +1030,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     ext: Schema.Attribute.String;
+    focalPoint: Schema.Attribute.JSON;
     folder: Schema.Attribute.Relation<'manyToOne', 'plugin::upload.folder'> &
       Schema.Attribute.Private;
     folderPath: Schema.Attribute.String &
